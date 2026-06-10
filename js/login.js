b171000db5307b433cb0ef4d04388958dc0c1206
Lucho7896 <luchosale96@gmail.com>
2026-06-10 19:24:07 -0300

    Se completó la lógica del register y el login. Modificación del CSS y colocación de imagenes para una mejor decoracion abusa

--- js/login.js
+++ js/login.js
@@ -1,7 +1,28 @@
+
+function mostrarNotificacion(texto, tipo = "error") {
+    const contenedor = document.getElementById("toastContainer");
+    
+
+    const toast = document.createElement("div");
+    toast.className = `toast-alerta ${tipo}`;
+    
+    // Le agregamos un ícono sutil según el tipo
+    const icono = tipo === "error" ? "❌" : "✅";
+    toast.innerHTML = `<span>${icono}</span> <span>${texto}</span>`;
+    
+    contenedor.appendChild(toast);
+    
+    setTimeout(() => {
+        toast.classList.add("salida");
+        setTimeout(() => {
+            toast.remove();
+        }, 300);
+    }, 3500);
+}
+
 const inputCorreo = document.getElementById("usuario");
 const inputContrasenia = document.getElementById("contraseña");
 const btnIngresar = document.getElementById("btnIngresar");
-const mensaje = document.getElementById("mensaje");
 
 btnIngresar.addEventListener("click", async () => {
 
@@ -9,54 +30,41 @@ btnIngresar.addEventListener("click", async () => {
     const contrasenia = inputContrasenia.value.trim();
 
     if (correo === "" || contrasenia === "") {
-        mensaje.textContent = "Completa todos los campos";
-        mensaje.className = "error";
+        mostrarNotificacion("Completa todos los campos", "error");
         return;
     }
 
     try {
-
         const respuesta = await axios.get("http://localhost:3000/usuarios");
-
         const usuarios = respuesta.data;
 
         const usuarioEncontrado = usuarios.find(usuario =>
             usuario.correo === correo
         );
 
-       
         if (!usuarioEncontrado) {
-            mensaje.textContent = "Correo incorrecto";
-            mensaje.className = "error";
+            mostrarNotificacion("Correo incorrecto", "error");
             return;
         }
 
-        
         if (usuarioEncontrado.contrasenia !== contrasenia) {
-            mensaje.textContent = "Contraseña incorrecta";
-            mensaje.className = "error";
+            mostrarNotificacion("Contraseña incorrecta", "error");
             return;
         }
 
-       
         localStorage.setItem(
             "usuarioLogueado",
             JSON.stringify(usuarioEncontrado)
         );
 
-        mensaje.textContent = "Login exitoso";
-        mensaje.className = "exito";
+        mostrarNotificacion("Login exitoso", "exito");
 
         setTimeout(() => {
-            window.location.href = "index.html";
+            window.location.href = "index.html"; 
         }, 1500);
 
     } catch (error) {
-
         console.error(error);
-
-        mensaje.textContent = "Error al conectar con el servidor";
-        mensaje.className = "error";
+        mostrarNotificacion("Error al conectar con el servidor", "error");
     }
-
 });
